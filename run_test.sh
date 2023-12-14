fa33f525f644c294b0b7b0fea485b01a23a03641
pyama <pyama@pepabo.com>
2023-12-14 11:17:27 +0900

    テスト拡充した

--- run_test.sh
+++ run_test.sh
@@ -10,11 +10,11 @@ oneTimeSetUp() {
 }
 
 oneTimeTearDown() {
-  docker rm -f nginx-httpbin
-  pkill nginx
+  docker rm -f nginx-httpbin | t
+  pkill nginx | true
 }
 
-test_delete_@refix() {
+test_delete_prefix() {
   local result=$(curl -s -b'name=example_prefix_a; value=1' -b'name=not_match_prefix_b; value=2;' http://localhost:1234/get -L -i)
   assertContains "$result" 'name=a; value=1'
   assertContains "$result" 'name=not_match_prefix_b; value=2'
@@ -23,6 +23,43 @@ test_delete_@refix() {
 test_append_prefix() {
   local result=$(curl http://localhost:1234/cookies/set/foo/bar -L -i)
   assertContains "$result" 'example_prefix_foo=bar; Path=/'
+}
+
+test_delete_long_cookie_with_request() {
+  local long_value=$(printf 'a%.0s' {1..1000}) # 1000文字の 'a' で構成される文字列
+  local result=$(curl -s -b"name=example_prefix_$long_value; value=1" http://localhost:1234/get -L -i)
+  assertContains "$result" "name=$long_value; value=1"
+}
+
+test_delete_many_cookies_with_request() {
+  local cookies=()
+  for i in {1..100}; do
+    cookies+=("-b" "name=example_prefix_name${i}; value=${i}")
+  done
+
+  local result=$(curl -s "${cookies[@]}" http://localhost:1234/get -L -i)
+  for i in {1..100}; do
+    assertContains "$result" "name=name${i}; value=${i}"
+  done
+}
+
+test_many_cookies_with_response() {
+  local query_params=""
+  for i in $(seq 1 100); do
+    query_params+="name${i}=value${i}&"
+  done
+
+  local result=$(curl -s -i -L "http://localhost:1234/cookies/set?$query_params")
+  for i in $(seq 1 100); do
+    assertContains "$result" "Set-Cookie: example_prefix_name${i}=value${i}; Path=/"
+  done
+}
+
+
+test_large_cookie_value_with_response() {
+  local large_value=$(printf 'a%.0s' {1..4000}) # 4000文字の 'a' で構成される文字列
+  local result=$(curl -s -i -L "http://localhost:1234/cookies/set?large_cookie=$large_value")
+  assertContains "$result" "Set-Cookie: example_prefix_large_cookie=$large_value; Path=/"
 }
 
 . tmp/shunit2/shunit2
